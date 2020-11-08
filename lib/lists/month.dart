e79b7270091ec5a5b3765cc51adba83c3a3e72e9
JenniferRuiz2 <jennifer_ruiz@arpan.com.mx>
2020-11-07 19:58:07 -0600

    Create month.dart

---
+++ lib/lists/month.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MonthListState extends StatelessWidget {
+
+  List<Container> indicators = [];
+  int currentPage = 0;
+
+  Widget updateIndicators(){
+    return Row(
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
