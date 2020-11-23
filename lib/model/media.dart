88c47772f84f5f8d8d40bc4a324679f6de4aa068
RamiroMendozaSanchez <ramiro_mendoza@arpan.com.mx>
2020-11-23 14:55:42 -0600

    Create media.dart

---
+++ lib/model/media.dart
@@ -0,0 +1,25 @@
+class Media{
+   int id;
+   String name;
+   String descripcion;
+   String imagLugar;
+   String videoLugar;
+   String audioLugar;
+   double long;
+   double lat;
+
+   factory Media(Map jsonMap){
+    return new Media.deserialize(jsonMap);
+   }
+
+   Media.deserialize(Map json):
+         id = json["id"].toInt(),
+         name = json["nombre"],
+         descripcion = json["descripcin"],
+         imagLugar= json["fotoLugar"],
+         videoLugar = json["video"],
+         audioLugar = json["audio"],
+         long = json["longitud"].toDouble(),
+         lat = json["latitud"].toDouble();
+
+}
